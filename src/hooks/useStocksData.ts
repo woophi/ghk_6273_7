7938312ba0eb3652771be7354d1201fa146cf6f6
Konstantin Mikheev <mik.kon6610@gmail.com>
2025-09-04 11:47:11 +0200

    init

---
+++ src/hooks/useStocksData.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import goldImg from '../assets/gold.png';
+import lukolImg from '../assets/lukol.png';
+import neftImg from '../assets/neft.png';
+import sberImg from '../assets/sber.png';
+import tbankImg from '../assets/tbank.png';
+import yandexImg from '../assets/yandex.png';
+import { StockItem } from '../types';
+
+export const TICKER_TO_IMAGE: Record<string, string> = {
+  SBER: sberImg,
+  T: tbankImg,
+  PLZL: goldImg,
+  YNDX: yandexImg,
+  TRNFP: neftImg,
+  LKOH: lukolImg,
+};
+
+export const useStocksData = () => {
+  const [stocks, setStocks] = useState<StockItem[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://gist.githubusercontent.com/nsdooris/47f58504a837227f958a026649960aac/raw/');
+      const data = (await response.json()) as { stocks: StockItem[] };
+      setStocks(
+        data.stocks.map(item => ({ ...item, delta: item.delta.replace('%', ''), img: TICKER_TO_IMAGE[item.ticker] })),
+      );
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  return { stocks, loading };
+};
